7665819e2b080ccc2c8221dc32ad24840aa26232
FrancesVugts <francesvugts@hotmail.com>
2020-10-25 14:02:00 +0000

    fix sku names for the houses and add HouseAdmin to admin.py in the houses app

--- houses/admin.py
+++ houses/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 from .models import House, Type
 
-# Register your models here.
-admin.site.register(House)
+
+class HouseAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'type',
+        'address',
+        'postal_code',
+        'city',
+        'price',
+        'start_date',
+        'end_date',
+    )
+
+    ordering = ('-sku',)
+
+
+admin.site.register(House, HouseAdmin)
 admin.site.register(Type)
