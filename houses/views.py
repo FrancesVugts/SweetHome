68fe4eef162b60cb56cf029b743de4399dde96f4
FrancesVugts <francesvugts@hotmail.com>
2020-11-07 10:25:24 +0000

    add type filter

--- houses/views.py
+++ houses/views.py
@@ -1,21 +1,28 @@
 from django.shortcuts import render, get_object_or_404
-from .models import House, City
+from .models import House, Type, City
 
 
 def view_houses(request):
     """ A view to render the houses page """
 
     houses = House.objects.all()
+    types = Type.objects.all()
     cities = City.objects.all()
-    city_name = None
+    city_name_query = None
+    type_name_query = None
 
     if 'city_name' in request.GET:
-        city_name = request.GET['city_name'].split(',')
-        houses = houses.filter(city__name__in=city_name)
+        city_name_query = request.GET['city_name']
+        houses = houses.filter(city__name__icontains=city_name_query)
+
+    if 'type_name' in request.GET:
+        type_name_query = request.GET['type_name']
+        houses = houses.filter(house_type__name__icontains=type_name_query)
 
     context = {
         'houses': houses,
         'cities': cities,
+        'types': types,
     }
 
     return render(request, 'houses/houses.html', context)
