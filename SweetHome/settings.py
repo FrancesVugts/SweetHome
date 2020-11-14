d21d86995e71e28179ef24821df81490dee7969c
FrancesVugts <francesvugts@hotmail.com>
2020-11-14 10:39:31 +0000

    Install crispy and adjust allauth layout

--- SweetHome/settings.py
+++ SweetHome/settings.py
@@ -48,6 +48,9 @@ INSTALLED_APPS = [
     'info',
     'contact',
     'profiles',
+
+    #Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -61,6 +64,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'SweetHome.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -79,6 +84,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
