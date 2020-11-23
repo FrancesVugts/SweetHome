0449b8fae13daeaeaf0f40de0280355562dbda9d
FrancesVugts <francesvugts@hotmail.com>
2020-11-23 11:00:16 +0000

    Product admin - Fix image field

--- houses/forms.py
+++ houses/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .widgets import CustomClearableFileInput
 from .models import House
 
 
@@ -7,6 +8,12 @@ class HouseForm(forms.ModelForm):
     class Meta:
         model = House
         fields = '__all__'
+
+    photo_1 = forms.ImageField(label='photo 1', required=False, widget=CustomClearableFileInput)
+    photo_2 = forms.ImageField(label='photo 2', required=False, widget=CustomClearableFileInput)
+    photo_3 = forms.ImageField(label='photo 3', required=False, widget=CustomClearableFileInput)
+    photo_4 = forms.ImageField(label='photo 4', required=False, widget=CustomClearableFileInput)
+    photo_5 = forms.ImageField(label='photo 5', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
